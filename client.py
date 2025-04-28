84df2b3f385b5a575bf3a1a95f3cdab23807dfb2
dawc17 <dawid.czaplicki2008@gmail.com>
2025-04-28 22:15:14 +0200

    done

--- client.py
+++ client.py
@@ -1,1 +1,15 @@
-print("hello client")
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(('localhost', 12345))
+done = False
+while not done:
+    client.send(input("Enter message: ").encode("utf-8"))
+    msg = client.recv(1024).decode("utf-8")
+    if msg == "exit":
+        done = True
+    else:
+        print(msg)
+
+client.close()
--- server.py
+++ server.py
@@ -1,1 +1,20 @@
-print("hello server")
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 12345))
+server.listen()
+
+client, address = server.accept()
+done = False
+
+while not done:
+    msg = client.recv(1024).decode("utf-8")
+    if msg == "exit":
+        done = True
+    else:
+        print(msg)
+    client.send(input("Enter message: ").encode("utf-8"))
+
+client.close()
+server.close()
+
